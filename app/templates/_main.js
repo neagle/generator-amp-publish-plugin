2d78853e8df4e397c9d1fc369a82030a7c247e15
Nate Eagle <n.eagle@gmail.com>
2014-05-07 15:20:49 -0400

    Fix #1 and #2

--- app/templates/_main.js
+++ app/templates/_main.js
@@ -5,9 +5,6 @@ $(function () {
 	'use strict';
 
 	// Build your <%= type %>!
-<% if (hidden) { %>
-	// Hide the actual plugin box, since we're doing everything via JS / CSS
-	$('.pluginbox h2:contains("<%= name %>")').closest('.pluginbox').hide();<% } %>
 
 	// Save a reference to our <%= type %>
 	var $container = $('#<%= type %>-<%= _.slugify(name) %>');
